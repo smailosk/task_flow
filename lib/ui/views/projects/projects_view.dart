e5e0da6586788ec2e7a5ba71cc62096e9eecf46e
Ismail Amor <ismail.amor@it-objects.de>
2024-02-03 15:53:43 +0100

    add default projects view

---
+++ lib/ui/views/projects/projects_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'projects_viewmodel.dart';
+
+class ProjectsView extends StatelessWidget {
+  const ProjectsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<ProjectsViewModel>.reactive(
+      viewModelBuilder: () => ProjectsViewModel(),
+      builder: (context, model, child) => Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Container(
+          padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        ),
+      ),
+    );
+  }
+}
