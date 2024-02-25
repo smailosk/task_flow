8afa53fb64dd0d57dd585bbf459aa3a43643ceda
KhalilAmor <khalilamor95@gmail.com>
2024-02-25 06:33:01 +0200

    Search Tasks

--- lib/ui/views/search/search_view.dart
+++ lib/ui/views/search/search_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stacked/stacked.dart';
+import 'package:task_flow/core/models/task/task.dart';
+import 'package:task_flow/core/utils/utils.dart';
 import 'package:task_flow/ui/common/ui_helpers.dart';
+import 'package:task_flow/ui/common/widgets/profile_picture.dart';
 
 import 'search_viewmodel.dart';
 
 class SearchView extends StatelessWidget {
-  const SearchView({Key? key}) : super(key: key);
+  const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +36,12 @@ class SearchView extends StatelessWidget {
             left: 20,
             right: 20,
           ),
-          child: Column(
+          child: ListView(
             children: [
               TextField(
+                onChanged: (value) {
+                  model.search(value);
+                },
                 decoration: InputDecoration(
                   hintText: "Search...",
                   prefixIcon: const Icon(Icons.search),
@@ -47,20 +54,131 @@ class SearchView extends StatelessWidget {
                 ),
               ),
               verticalSpaceMedium,
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              if (model.searchResults.isNotEmpty)
+                ...List.generate(
+                  model.searchResults.length,
+                  (index) => SearchTaskCard(
+                    task: model.searchResults[index],
+                    taskIndex: index,
+                  ),
+                ),
+              if (model.searchResults.isEmpty)
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // show image in case
+                      SvgPicture.asset(
+                        'assets/images/search.svg',
+                        width: MediaQuery.of(context).size.width * 0.6,
+                      ),
+                    ],
+                  ),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchTaskCard extends ViewModelWidget<SearchViewModel> {
+  final TaskModel task;
+  final int taskIndex;
+
+  const SearchTaskCard({
+    super.key,
+    required this.task,
+    required this.taskIndex,
+  });
+
+  @override
+  Widget build(BuildContext context, SearchViewModel viewModel) {
+    return Card(
+      elevation: 3,
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Checkbox(
+                  value: task.done,
+                  onChanged: (value) =>
+                      viewModel.toggleTaskCompletion(taskIndex, value ?? false),
+                ),
+                Expanded(
+                  child: Text(
+                    task.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      decoration: task.done ? TextDecoration.lineThrough : null,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                // PopupMenuButton(
+                //   child: const Icon(
+                //     Icons.more_vert,
+                //     color: Colors.black,
+                //   ),
+                //   itemBuilder: (context) => [
+                //     const PopupMenuItem(value: 'edit', child: Text('Edit')),
+                //     const PopupMenuItem(value: 'delete', child: Text('Delete')),
+                //   ],
+                //   onSelected: (value) {
+                //     if (value == 'edit') viewModel.editTask(taskIndex);
+                //     if (value == 'delete') viewModel.deleteTask(taskIndex);
+                //   },
+                // ),
+              ],
+            ),
+            if (task.details.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 15,
+                ),
+                child: Text(
+                  task.details,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                    decoration: task.done ? TextDecoration.lineThrough : null,
+                  ),
+                ),
+              ),
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
                   children: [
-                    // show image in case
-                    SvgPicture.asset(
-                      'assets/images/search.svg',
-                      width: MediaQuery.of(context).size.width * 0.6,
+                    const Icon(Icons.access_time, size: 20),
+                    horizontalSpaceTiny,
+                    Text(
+                      Utils.formatDateTime(task.deadline!),
+                      style: const TextStyle(fontSize: 14),
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
+                if (task.assignee.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ProfilePicture(
+                      userId: task.assignee,
+                    ),
+                  ),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- lib/ui/views/search/search_viewmodel.dart
+++ lib/ui/views/search/search_viewmodel.dart
@@ -1,3 +1,54 @@
 import 'package:stacked/stacked.dart';
+import 'package:task_flow/app/app.locator.dart';
+import 'package:task_flow/app/app.logger.dart';
+import 'package:task_flow/core/error_handling/executor.dart';
+import 'package:task_flow/core/models/task/task.dart';
+import 'package:task_flow/services/repo_service.dart';
 
-class SearchViewModel extends BaseViewModel {}
+class SearchViewModel extends BaseViewModel {
+  final _repoService = locator<RepoService>();
+  final _log = getLogger('SearchViewModel');
+  init() {}
+
+  void search(String value) {
+    if (value.isEmpty) {
+      _searchResults.clear();
+      notifyListeners();
+      return;
+    }
+    notifyListeners();
+    final input = value.trim().toLowerCase();
+
+    final titleResults = _repoService.tasksByProject.values
+        .expand((e) => e.values)
+        .where((task) => task.title.toLowerCase().contains(value.toLowerCase()))
+        .toList();
+
+    final detailsResults = _repoService.tasksByProject.values
+        .expand((e) => e.values)
+        .where(
+            (task) => task.details.toLowerCase().contains(value.toLowerCase()))
+        .toList();
+
+    final finalResults = {...titleResults, ...detailsResults}.toSet().toList();
+    _searchResults.clear();
+    _searchResults.addAll(finalResults);
+    notifyListeners();
+
+    _log.i('search - $input - $finalResults');
+  }
+
+  final List<TaskModel> _searchResults = [];
+  List<TaskModel> get searchResults => _searchResults;
+
+  void toggleTaskCompletion(int index, bool doneValue) {
+    final task = _searchResults[index];
+
+    Executor.runFuture(_repoService.setTaskAsDone(task, doneValue))
+        .then((value) => value.fold((failure) {}, (r) {
+              _searchResults[index] = task.copyWith(done: doneValue);
+              notifyListeners();
+              _log.i('Marked as :${!task.done}');
+            }));
+  }
+}
